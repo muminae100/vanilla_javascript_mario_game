bbb24cf7b52e2d1517871939209e0fcfbfe78af2
foreverkenyan <smuminaetx100@gmail.com>
2022-04-16 21:59:04 +0300

    Implement player movement

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class Player {
     update(){
         this.draw();
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
         if(this.position.y + this.height +
             this.velocity.y <= canvas.height){
             this.velocity.y += gravity;
@@ -39,9 +40,68 @@ class Player {
 
 const player = new Player();
 
+const keys = {
+    right:{
+        pressed: false
+    },
+    left:{
+        pressed: false
+    }
+}
+
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
+
+    if(keys.right.pressed){
+        player.velocity.x = 5;
+    }
+    else if(keys.left.pressed){
+        player.velocity.x = -5;
+    }
+    else{
+        player.velocity.x = 0;
+    }
 }
 animate();
+
+window.addEventListener('keydown', ( { keyCode } )=>{
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = true;
+            console.log('This is left');
+            break;
+        case 68:
+            keys.right.pressed = true;
+            console.log('This is right');
+            break;
+        case 87:
+            player.velocity.y -= 20;
+            console.log('This is up');
+            break;
+    
+        default:
+            break;
+    }
+})
+
+window.addEventListener('keyup', ( { keyCode } )=>{
+    switch (keyCode) {
+        case 65:
+            keys.left.pressed = false;
+            console.log('This is left');
+            break;
+        case 68:
+            keys.right.pressed = false;
+            console.log('This is right');
+            break;
+        case 87:
+            player.velocity.y = 0;
+            console.log('This is up');
+            break;
+    
+        default:
+            break;
+    }
+})
